78d6a544c1a5afcdbd19f03cedec4e9e9fe23440
TimofeyPro <mail@timofey.pro>
2022-02-02 00:43:12 +0300

    Update schema.py

--- backend/blog/schema.py
+++ backend/blog/schema.py
@@ -10,31 +10,72 @@ UserType = get_user_model()
 
 @strawberry.django.type(models.Profile)
 class AuthorType:
-    user: str
-    website: str
-    bio: str
+    id: auto
+    user: auto
+    website: auto
+    bio: auto
 
 @strawberry.django.type(models.Tag)
-class Tag:
-    name: str
+class TagType:
+    id: auto
+    name: auto
   
 @strawberry.django.type(models.Post)
 class PostType:
-    title: str
-    subtitle: str
-    slug: str
-    body: str
-    meta_description: str
-    date_created: date
-    date_modified: date
-    publish_date: date
-    published: bool
+    id: auto
+    title: auto
+    subtitle: auto
+    slug: auto
+    body: auto
+    meta_description: auto
+    date_created: auto
+    date_modified: auto
+    publish_date: auto
+    published: auto
     author: 'AuthorType'
-    tags: List['Tag']
+    tags: List['TagType']
+
+def get_all_posts(root, info):
+    return(
+        models.Post.objects.prefetch_related('tags')
+        .select_related('author')
+        .all()
+    )
+
+def author_by_username(root, info, username: str):
+    return models.Profile.objects.select_related('user').get(
+        user__username = username
+    )
+
+def post_by_slug(root, info, slug: str):
+    return (
+        models.Post.objects.prefetch_related('tags')
+        .select_related('author')
+        .get(slug=slug)
+    )
+
+def posts_by_author(root, info, username: str):
+    return (
+        models.Post.objects.prefetch_related('tags')
+        .select_related("author")
+        .filter(author__user__username=username)
+    )
+
+def posts_by_tag(root, info, tag: str):
+    return (
+        models.Post.objects.prefetch_related('tags')
+        .select_related('author')
+        .filter(tags__name__iexact=tag)
+    )
 
 
 @strawberry.type
 class Query:
-    allPosts: List[PostType]= strawberry.django.field()
-
+    all_posts: List[PostType] = strawberry.django.field(resolver=get_all_posts)
+    author_by_username: List[AuthorType] = strawberry.django.field(resolver=author_by_username)
+    post_by_slug: List[PostType] = strawberry.django.field(resolver=post_by_slug)
+    posts_by_author: List[PostType] = strawberry.django.field(resolver=posts_by_author)
+    posts_by_tag: List[PostType] = strawberry.django.field(resolver=posts_by_tag)
+ 
 schema = strawberry.Schema(query=Query)
+
