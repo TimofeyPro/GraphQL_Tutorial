3383872361312efcd1e40bd82919d950e6cb4951
TimofeyPro <mail@timofey.pro>
2022-02-03 20:45:11 +0300

    Update schema_graphene.py

--- backend/blog/schema_graphene.py
+++ backend/blog/schema_graphene.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
 
 from blog import models
-
+import graphene
 
 class UserType(DjangoObjectType):
     class Meta:
